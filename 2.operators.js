4bbd2ecbce255bf59d8d895547d10791a086d7b4
Mahi Jakaria Alam <78194897+anticlockwise-999@users.noreply.github.com>
2021-06-20 13:58:53 +0600

    Add files via upload

---
+++ 2.operators.js
@@ -0,0 +1,26 @@
+
+//User input
+var x;
+x= prompt("enter your name: ");
+console.log(x);
+document.write(x);
+
+//Increment a++/++a
+var a= 5;
+var b=7;
+var c,d;
+
+d=++b;
+console.log(c);
+console.log(a);
+console.log(d);
+
+a=(3-5)*5/7+4*4;
+console.log(a);
+
+
+
+
+
+
+
