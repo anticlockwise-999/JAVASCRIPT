0aa95f14b1568aa3c455c0947d9946edfd266706
Mahi Jakaria Alam <78194897+anticlockwise-999@users.noreply.github.com>
2021-06-23 16:02:00 +0600

    Add files via upload

---
+++ 2.3 for loop.js
@@ -0,0 +1,19 @@
+var i=1;
+
+//while loop
+while (i <=5){
+    console.log(i);
+    i++;
+}
+console.log("End of the while loop")
+console.log("Start of for loop");
+
+//for loop
+for(var j=1; j<=4; j++){
+    console.log(j);
+}
+//first condition check, then print, then update
+
+for(var k=5; k>=1; k--){
+    console.log(k);
+}
