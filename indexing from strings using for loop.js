de8324caa73292b62fa43937c3048ef593d0fc33
Mahi Jakaria Alam <78194897+anticlockwise-999@users.noreply.github.com>
2021-06-23 17:21:11 +0600

    Add files via upload

---
+++ indexing from strings using for loop.js
@@ -0,0 +1,9 @@
+//string
+
+let name = "UEFA EURO";
+let len = name.length;
+
+for(var i=0; i<len; i++){
+    console.log(`Index:  ${i}`); //Index :0,1,2,3...
+    console.log(name[i]); //U,E,F,.....
+}
