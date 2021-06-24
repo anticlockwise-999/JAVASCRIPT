e696b166454af215bdf8fd6c2326d2910f9ee1ba
Mahi Jakaria Alam <78194897+anticlockwise-999@users.noreply.github.com>
2021-06-24 22:41:35 +0600

    Add files via upload

---
+++ 3.3 Function Expression and array function.js
@@ -0,0 +1,19 @@
+//Normal declaration:
+function one(name){
+    console.log('Adios' + name);
+}
+
+//Function expression:
+let two= function(name2){
+    console.log('Adios' + name2);
+}
+
+//Arrow function:
+let three =(name3) => {
+    console.log("I am"+ name3
+    );
+}
+
+one(" monsieur");
+two(" senorita");
+three(" vengeance")
