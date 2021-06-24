2794ba7ac07410d3c5c2be0aa84945bd5dfc771d
Mahi Jakaria Alam <78194897+anticlockwise-999@users.noreply.github.com>
2021-06-24 17:33:07 +0600

    Add files via upload

---
+++ 3.1 Function.js
@@ -0,0 +1,10 @@
+//Built in functions: alert() console.log()
+
+//user defined functions
+
+function saysomething(){
+    console.log("Hello!");
+    console.log("I am learning JS! ");
+    console.log("Right now")
+}
+saysomething();
