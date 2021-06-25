f0360442a1c8224acbf6a63138760e18c092bf67
Mahi Jakaria Alam <78194897+anticlockwise-999@users.noreply.github.com>
2021-06-26 00:54:38 +0600

    Add files via upload

---
+++ 4.3 Static function.js
@@ -0,0 +1,20 @@
+
+//Static function
+class person {  
+    constructor(fname, lname) { //function = constructor   fname= parameter
+        this.firstname = fname ; //property=firstname,
+        this.lastname = lname ;
+    }
+    greeting(){ //gretting = function or method
+        return `Hello ${this.firstname} ${this.lastname}`;
+    }
+    static test(){
+        console.log("I am static");
+    }
+}
+let person1 = new person("Mahi", "Jakaria");
+
+console.log(person1.greeting()); //prints Hello Mahi Jakaria
+//we have to call normal method from object
+console.log(person.test()); //prints I am static
+//we have to call static from class 
