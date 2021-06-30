6f4df157ed80e45fa997e67c317834e9ab4adf36
Mahi Jakaria Alam <78194897+anticlockwise-999@users.noreply.github.com>
2021-06-30 17:33:43 +0600

    Update 1.3 Switch.js

--- 1.3 Switch.js
+++ 1.3 Switch.js
@@ -21,5 +21,28 @@ switch(choice){
 console.log(text);
 
 
+//another one
 
+function caseInSwitch(val) {
+  var answer = "";
+  // Only change code below this line
+  switch (val) {
+    case 1:
+      return "alpha";
+      break;
+    case 2:
+      return "beta";
+      break;
+    case 3:
+      return "gamma";
+      break;
+    case 4:
+      return "delta";
+      break;
+  }
 
+  // Only change code above this line
+  return answer;
+}
+
+caseInSwitch(1);
