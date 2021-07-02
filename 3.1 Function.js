8c92f52e914178bda043af433adeb6b9105446c5
Mahi Jakaria Alam <78194897+anticlockwise-999@users.noreply.github.com>
2021-07-02 18:53:50 +0600

    Update 3.1 Function.js

--- 3.1 Function.js
+++ 3.1 Function.js
@@ -16,3 +16,11 @@ function convertToInteger(str) {
 return parseInt(str);}
 
 convertToInteger("56");
+
+
+//Use the parseInt Function with a Radix
+
+function convertToInteger(str) {
+  return parseInt(str,2); //The radix variable says that 10011 is in the binary system, or base 2. This example converts the string 10011 to an integer 19.
+}
+convertToInteger("10011");
